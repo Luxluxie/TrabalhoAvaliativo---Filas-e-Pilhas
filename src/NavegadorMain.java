05cce7d0f356532f755498390a7135563076feb0
Yuri <yuripeterson198@gmail.com>
2026-04-05 14:38:41 -0300

    Inclusão de Classes

---
+++ src/NavegadorMain.java
@@ -0,0 +1,5 @@
+public class NavegadorMain {
+    public static void main(String[] args) {
+       return;
+    }
+}
---
+++ src/SistemaSenhas.java
@@ -0,0 +1,5 @@
+public class SistemaSenhas {
+        public static void main(String[] args) {
+            return;
+        }
+}
